7f364f25caadc720cb087c89339da19c7f67558e
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-01-02 22:02:42 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "scala212-cats-category-theory-composing-functors"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+scalacOptions += "-Ypartial-unification"
+
+libraryDependencies += "org.typelevel" %% "cats-core" % "1.5.0"
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.5"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
---
+++ src/test/scala/FunctorCompositionTest.scala
@@ -0,0 +1,18 @@
+import cats.Functor
+import org.scalatest.{FunSuite, Matchers}
+import cats.instances.list._
+import cats.instances.option._
+
+/**
+  * Created by mtumilowicz on 2019-01-02.
+  */
+class FunctorCompositionTest extends FunSuite with Matchers {
+  
+  test("Composing functors is all about composing appropriate map functions") {
+    def square(i: Int) = i * i
+    val optionList = Option(List(1, 2, 3))
+    
+    Functor[Option].compose[List].map(optionList)(square) should be (Some(List(1, 4, 9)))
+  }
+  
+}
